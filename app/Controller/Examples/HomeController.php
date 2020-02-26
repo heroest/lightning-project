09bb0d6c72e97c5f3677c79353f7d64e74bb71c2
zhangdonghai <zhangdonghai@peilian.com>
2020-02-26 18:43:46 +0800

    tiny changes on examples

--- app/Controller/Examples/HomeController.php
+++ app/Controller/Examples/HomeController.php
@@ -9,12 +9,13 @@ class HomeController
 {
     public function helloWorldAction(Input $input, Output $output)
     {
-        $name = $input->getPost('name', 'lightning');
+        $name = $input->post('name', 'lightning');
+        $raw = $input->rawPost();
         $output->setData(
             Output::TYPE_JSON,
             [
                 'code' => 200, 
-                'data' => ['ip' => $input->getClientIp()], 
+                'data' => ['ip' => $input->getClientIp(), 'raw' => $raw],
                 'msg' => "hello-world-from-{$name}"
             ]
         );
